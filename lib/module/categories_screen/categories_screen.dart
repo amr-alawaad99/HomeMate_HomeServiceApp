8751af8cc40a2b2029fe0ee3e0ec7df960eb7355
ahmed ramadan <ahmed.ramadan20@outlook.com>
2023-03-13 16:28:52 +0200

    updating categories screen

--- lib/module/categories_screen/categories_screen.dart
+++ lib/module/categories_screen/categories_screen.dart
@@ -1,16 +1,218 @@
 import 'package:flutter/material.dart';
 
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(
-          child: Text("Categories"),
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Material(
+                  elevation: 3,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: 100.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/cleaning.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Cleaning'),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Material(
+                  elevation: 3,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: 100.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/kitchen.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Kitchen'),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Material(
+                  elevation: 3,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: 100.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/plumbing.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Plumbing'),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Material(
+                  elevation: 3,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: 100.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/paint.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Paint'),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: Material(
+                  elevation: 3,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: 100.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/carpentry.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Carpentry'),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 15,
+              ),
+              Expanded(
+                child: Material(
+                  elevation: 3,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  child: Container(
+                    height: 120,
+                    width: 100.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image(
+                          image: AssetImage('assets/images/electrician.png'),
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Electricity'),
+                      ],
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
