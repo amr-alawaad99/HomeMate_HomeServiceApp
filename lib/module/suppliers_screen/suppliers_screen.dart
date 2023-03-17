eb6bb747364e80afa8f1db189d2188c6fd3a0b61
Mahmoud AliX <m.ronaldo9981@gmail.com>
2023-03-17 14:27:48 +0200

    committed

--- lib/module/suppliers_screen/suppliers_screen.dart
+++ lib/module/suppliers_screen/suppliers_screen.dart
@@ -2,31 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../shared/components/components.dart';
 
-/*Padding(
-padding: const EdgeInsets.only(top: 20, right: 20, left: 20),
-child: Container(
-width: double.infinity,
-height: 30,
-child: ListView.separated(
-itemBuilder: (BuildContext context, int index) =>
-defaultCategoryChooser(text: 'Cleaning'),
-separatorBuilder: (context, index) => SizedBox(
-width: 5,
-),
-itemCount: 6,
-shrinkWrap: true,
-scrollDirection: Axis.horizontal,
-),
-),
-),*/
-/*Container(
-height: MediaQuery.of(context).size.height,
-child: ListView.builder(
-scrollDirection: Axis.vertical,
-shrinkWrap: true,
-itemBuilder: (context, index) => SuppliersItem(),
-itemCount:10),
-),*/
+
 class SuppliersScreen extends StatelessWidget {
   const SuppliersScreen({Key? key}) : super(key: key);
 
