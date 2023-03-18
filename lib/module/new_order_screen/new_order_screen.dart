1b18a8ba75c1ed9e530788edeba4c53e119af809
ahmed ramadan <ahmed.ramadan20@outlook.com>
2023-03-18 10:23:13 +0200

    update new_order_screen.dart

--- lib/module/new_order_screen/new_order_screen.dart
+++ lib/module/new_order_screen/new_order_screen.dart
@@ -1,5 +1,7 @@
 import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:intl/intl.dart';
 
 import '../../layout/cubit/cubit.dart';
 import 'package:flutter/material.dart';
@@ -10,88 +12,74 @@ import '../../shared/components/constants.dart';
 
 class NewOrderScreen extends StatelessWidget {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   var dateController = TextEditingController();
 
   NewOrderScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => LayoutCubit(),
-      child: BlocConsumer<LayoutCubit, LayoutStates>(
-        listener: (context, state) {},
-        builder: (context, state) {
-          return ColorfulSafeArea(
-            color: primaryColor,
-            child: Scaffold(
-              key: scaffoldKey,
-              extendBody: true,
-              body: NestedScrollView(
-                floatHeaderSlivers: true,
-                headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
-                  const SliverAppBar(
-                      title: Text(
-                        'New Order',
-                        style: TextStyle(
-                            color: Colors.white, fontFamily: "Roboto"),
-                      ),
-                      backgroundColor: primaryColor,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.vertical(
-                              bottom: Radius.circular(15))),
-                      automaticallyImplyLeading: true,
-                      leadingWidth: 50,
-                      iconTheme: IconThemeData(color: Colors.white),
-                      bottom: PreferredSize(
-                          preferredSize: Size.fromHeight(20),
-                          child: SizedBox())),
-                ],
-                body: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text('Select Service'),
-                      SizedBox(
-                        height: 125,
-                        width: double.infinity,
-                        child: ListView.separated(
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, index) => Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 10),
-                                  child: defaultCategoriesBox(
-                                      text: 'Cleaning',
-                                      img: 'assets/images/cleaning.png',
-                                      width: 120,
-                                      height: 120),
-                                ),
-                            separatorBuilder: (context, index) => SizedBox(
-                                  width: 10,
-                                ),
-                            itemCount: 6),
-                      ),
-                      defaultTextFromField(
-                          hintText: 'Choose Working Day',
-                          controller: dateController,
-                          keyboardType: TextInputType.datetime,
-                          onTapFunction: () {
-                            showDatePicker(
-                              context: context,
-                              initialDate: DateTime.now(),
-                              firstDate: DateTime.now(),
-                              lastDate: DateTime(2123),
-                            ).then((value) {
-                              dateController.text = value.toString();
-                            });
-                          })
-                    ],
-                  ),
+    return ColorfulSafeArea(
+      color: primaryColor,
+      child: Scaffold(
+        key: scaffoldKey,
+        extendBody: true,
+        body: NestedScrollView(
+          floatHeaderSlivers: true,
+          headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
+            const SliverAppBar(
+                title: Text(
+                  'New Order',
+                  style: TextStyle(color: Colors.white, fontFamily: "Roboto"),
                 ),
+                backgroundColor: primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(15))),
+                automaticallyImplyLeading: true,
+                leadingWidth: 50,
+                iconTheme: IconThemeData(color: Colors.white),
+                bottom: PreferredSize(
+                    preferredSize: Size.fromHeight(20), child: SizedBox())),
+          ],
+          body: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  const Text('Select Service'),
+                  const SizedBox(height: 15,),
+
+                  defaultTextFromField(
+                    validator: (String? value) {
+                      if(value!.isEmpty){
+                        return 'you must choose the day';
+                      }
+                      return null;
+                    },
+                      hintText: 'Choose Working Day',
+                      prefixIcon: TablerIcons.calendar_event,
+                      controller: dateController,
+                      keyboardType: TextInputType.datetime,
+                      onTapFunction: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime.now(),
+                          lastDate: DateTime(2123),
+                        ).then((value) {
+                          dateController.text = DateFormat.yMMMEd().format(value!);
+
+                        });
+                      })
+                ],
               ),
             ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
